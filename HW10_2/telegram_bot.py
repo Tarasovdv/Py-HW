84d3b3f74b73f5b99120b64c5a6b113ee8562837
Dima Tarasov <desant74268@yandex.ru>
2022-11-15 11:07:07 +0300

    HW10_2 bot

---
+++ HW10_2/telegram_bot.py
@@ -0,0 +1,17 @@
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+from bot_commands import*
+
+
+
+
+app = ApplicationBuilder().token("5662688007:AAEMEl8VaWiUkBmpdsvo3aYJ9Wp8JArsI1Q").build()
+
+# app.add_handler(CommandHandler("hi",))
+app.add_handler(CommandHandler("hi", hi_command))
+app.add_handler(CommandHandler("time", time_command))
+app.add_handler(CommandHandler("help", help_command))
+app.add_handler(CommandHandler("sum", sum_command))
+
+print("server started")
+app.run_polling()
